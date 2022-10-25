403f3d99f4fa7bd8e4eeb3a819526a7118dce2a5
Camilla Souto <104527029+Camis-souto@users.noreply.github.com>
2022-10-25 11:28:56 -0300

    Deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/users')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     // esta apenas redirecionando o usuário para o serviço correto.
-    return this.appService.getHello();
+    return this.appService.getHello();}
   }
-  @Get('/login')
-  getLogin(): string {
-    // esta apenas redirecionando o usuário para o serviço correto.
-    return this.appService.getLogin();
-  }
-  @Get('/register')
-  getRegister(): string {
-    // esta apenas redirecionando o usuário para o serviço correto.
-    return this.appService.getRegister();
-  }
-}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Bem vindo à API avanade!';
   }
-  getLogin(): string {
-    return 'Aqui virá o login do usuário';
-  }
-  getRegister(): string {
-    return 'Aqui virá o cadastro do usuário';
-  }
+ 
 }
