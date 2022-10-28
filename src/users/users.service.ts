459b95374813f54623f1c05b83d750b5f5215260
Camilla Souto <104527029+Camis-souto@users.noreply.github.com>
2022-10-28 13:42:20 -0300

    'Deploy

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { CreateUserDTO } from './dto/createUser.dto';
 import { UpdateUserDTO } from './dto/updateUser.dto';
+import { PrismaService } from '../prisma.service';
+
 
 @Injectable()
 export class UsersService {
-    async create(): Promise<String> {
-        return 'Usuário criado com sucesso!';
+    constructor(private prisma: PrismaService){}
+
+    async create(data: CreateUserDTO) {
+        const {name, email, password } = data;
+        const user = await this.prisma.users.create({
+            data: {
+                name,
+                email, //quando é o mesmo nome, não precisa colocar email: email. mas somente quando for exatemente igual
+                password,
+            },
+        });
+        if (!user) {
+            throw new HttpException(
+              {
+                status: HttpStatus.FORBIDDEN,
+                message: 'Erro ao criar usuário!',
+              },
+              HttpStatus.FORBIDDEN,
+            );
+          }
+        return user;
     }
     async findAll(): Promise<String> {
         return 'Lista de usuários!';
